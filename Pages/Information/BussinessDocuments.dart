68e9db6b938065ee80186c1b494c79132e70ea0c
DKsanjana <67433315+DKsanjana@users.noreply.github.com>
2020-11-02 22:44:49 +0530

    Add files via upload

---
+++ Pages/Information/BussinessDocuments.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BussinessDocuments extends StatefulWidget {
+  @override
+  _BussinessDocumentsState createState() => _BussinessDocumentsState();
+}
+
+class _BussinessDocumentsState extends State<BussinessDocuments> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
